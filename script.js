394731a511916870c22426da5441575185ea0099
cyptrid <cyptrid@gmail.com>
2020-12-25 00:29:31 +0700

    DOM Traversal (Result)

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-//DOM Traversal
+//Event Handling
+const close = document.querySelectorAll('.close');
 
+    // for( let i = 0; i < close.length; i++){
+    //     close[i].addEventListener('click', function(e){
+    //         // close[i].parentElement.style.display = 'none';
+    //         e.target.parentElement.style.display = 'none';
+    //     });
+    // }
 
+    //Membuat lebih ringkas lagi
+    close.forEach(function(el){
+    el.addEventListener('click', function(e){
+        e.target.parentElement.style.display = 'none';
+    });
+});
