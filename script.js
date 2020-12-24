3ebb4a0c3a8d42c3da7c19560c7e97c739d0e26c
cyptrid <cyptrid@gmail.com>
2020-12-24 23:15:37 +0700

    DOM Traversal (default)

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-//DOM Event
-//Event Handler
-const p3 = document.querySelector('.p3');
-
-function ubahWarna(){
-    p3.style.backgroundColor = 'lightblue';
-}
-
-//addEventListener
-const p4 = document.querySelector('section#b p');
-p4.addEventListener('click', function(){
-    const ul = document.querySelector('section#b ul');
-    const liBaru = document.createElement('li');
-    const teksliBaru = document.createTextNode('item baru');
-    liBaru.appendChild(teksliBaru);
-    ul.appendChild(liBaru); 
-});
-
+//DOM Traversal
 
 
