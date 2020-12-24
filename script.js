62aa96f5166ad4dbe8d04cc8e193a8f3ed8e47ec
cyptrid <cyptrid@gmail.com>
2020-12-24 17:04:24 +0700

    DOM Selection + script.js (Result 2)

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 //DOM SELECTION
 //document.getElementById() -> element
-const judul = document.getElementById('judul');
-judul.style.color = 'red';
-judul.style.backgroundColor = 'grey';
-judul.innerHTML = 'Ikhdanul Widad Joban'
-
 //document.getElementsByTagName() -> HTMLCollections
-const p = document.getElementsByTagName('p');
-for(let i = 0; i < p.length; i++){
+//document.getElementsByClassName() -> HTMLCollections
+
+
+//document.querySelector()
+const p4 = document.querySelector('#b p');
+p4.style.color = 'green';
+p4.style.fontSize = '30px';
+
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'orange';
+
+
+//document.querySelectorAll();
+const p = document.querySelectorAll('p');
+for (let i = 0; i < p.length; i++){
+    p[i].innerHTML = 'ini dirubah dari javascript';
     p[i].style.backgroundColor = 'lightblue';
 }
 
-//document.getElementsByClassName() -> HTMLCollections
-const p1 = document.getElementsByClassName('p1');
-p1[0].innerHTML = 'Ini dirubah dari javascript';
+// Scope DOM
+
+const sectionB = document.querySelector('section#b');
+const p5 = sectionB.getElementsByTagName('li')[0];
+p5.style.backgroundColor = 'green';
