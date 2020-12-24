e6eb7e0d6bda54daea89e193661084d53ef6987a
cyptrid <cyptrid@gmail.com>
2020-12-24 23:09:32 +0700

    DOM Event

--- script.js
+++ script.js
@@ -1,41 +1,20 @@
-//DOM Manipulation
+//DOM Event
+//Event Handler
+const p3 = document.querySelector('.p3');
 
-// SetAttribute
-const judul1 = document.getElementsByTagName('h1')[0];
-judul1.setAttribute('name', 'joban');
- 
-//contoh buat elemen Baru
-const pBaru = document.createElement('p');
-const teksPBaru = document.createTextNode('Paragraf Baru');
-//simpan tulisan ke dalam paragraf
-pBaru.appendChild(teksPBaru);
-//simpan pBaru di akhir section A
-const sectionA = document.getElementById('a');
-sectionA.append(pBaru);
+function ubahWarna(){
+    p3.style.backgroundColor = 'lightblue';
+}
 
-//contoh 2
-const liBaru = document.createElement('li');
-const teksLiBaru = document.createTextNode('Item Baru');
-liBaru.appendChild(teksLiBaru);
-
-const ul = document.querySelector('section#b ul');
-const li2 = ul.querySelector('li:nth-child(2)'); 
-ul.insertBefore(liBaru, li2);
-
-const link = document.getElementsByTagName('a')[0];
-sectionA.removeChild(link);
-
-const sectionB = document.getElementById('b');
-//node lama
-const p4 = sectionB.querySelector('p');
-//node baru
-const h2Baru = document.createElement('h2');
-const teksh2Baru = document.createTextNode('judul Baru');
-h2Baru.appendChild(teksh2Baru);
-
-sectionB.replaceChild(h2Baru, p4)
+//addEventListener
+const p4 = document.querySelector('section#b p');
+p4.addEventListener('click', function(){
+    const ul = document.querySelector('section#b ul');
+    const liBaru = document.createElement('li');
+    const teksliBaru = document.createTextNode('item baru');
+    liBaru.appendChild(teksliBaru);
+    ul.appendChild(liBaru); 
+});
 
 
-pBaru.style.backgroundColor = 'lightgreen';
-liBaru.style.backgroundColor = 'lightgreen';
-h2Baru.style.backgroundColor = 'lightgreen';
+
